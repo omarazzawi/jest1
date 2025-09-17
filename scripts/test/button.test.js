5ae44d0f5b5f4d827f025890bc2bf79f4080c93d
omarazzawi <omar.alazzawi1987@gmail.com>
2025-09-17 20:49:32 +0200

    Extra test for DOM

--- scripts/test/button.test.js
+++ scripts/test/button.test.js
@@ -1,15 +1,24 @@
 /**
  * @jest-environment jsdom
  */
+
 const buttonClick = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
+
 describe("DOM tests", () => {
     test("Expects content to change", () => {
         buttonClick();
         expect(document.getElementById("par")
             .innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
